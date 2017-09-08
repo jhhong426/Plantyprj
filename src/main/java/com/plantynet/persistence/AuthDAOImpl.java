a92566c596aca7ca492a327b91d8692fa812c507
zajanksk <zajan91@naver.com>
2017-09-08 10:05:38 +0900

    2. 김규식 2차 merge
    -2차 merge commit

---
+++ src/main/java/com/plantynet/persistence/AuthDAOImpl.java
@@ -0,0 +1,20 @@
+package com.plantynet.persistence;
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AuthDAOImpl implements AuthDAO {
+
+	@Autowired
+	private SqlSession sqlSession;
+	private static final String namespace = "com.plantynet.mapper.AuthMapper";
+	
+	@Override
+	public String getSuperPassword() throws Exception {
+		return sqlSession.selectOne(namespace+".getSuperPassword");
+	}
+
+}
--- src/main/java/com/plantynet/persistence/VoteDAO.java
+++ src/main/java/com/plantynet/persistence/VoteDAO.java
@@ -8,16 +8,16 @@ import com.plantynet.domain.VoteVO;
 
 public interface VoteDAO {
 	
-	public List<SurveyVO> getAllSurvey();
+	public List<SurveyVO> getAllSurvey() throws Exception;							// 전체 질문 글을 가져오는 메소드
 	
-	public List<VoteVO> getAllVotes();
+	public List<VoteVO> getAllVotes() throws Exception;								// 전체 투표를 가져오는 메소드
 	
-	public void blindSurvy(Integer survey_no);
+	public void blindSurvy(Integer survey_no) throws Exception;						// 해당 질문 글을 블라인드 처리 하는 메소드
 	
-	public void delSurvy(Integer survey_no);
+	public void delSurvy(Integer survey_no) throws Exception;						// 해당 질문 글을 삭제하는 메소드
 	
-	public SurveyVO getSurvey(Integer survey_no);
+	public SurveyVO getSurvey(Integer survey_no) throws Exception;					// 하나의 질문 글을 가져오는 메소드
 	
-	public SurResultVO getSurResult(Integer survey_no);
+	public SurResultVO getSurResult(Integer survey_no) throws Exception;				// 해당 질문 글의 결과 통계룰 가져오는 메소드
 	
 }
